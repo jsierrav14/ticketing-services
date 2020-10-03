5150731d9632d2b081f6378dd08215339d8da81b
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2020-10-02 23:15:08 -0500

    my custom userequest

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,28 +1,16 @@
 import { useState } from 'react'
 import axios from 'axios'
+import useRequest from '../../hooks/use-request'
 export default () => {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [errors, setErrors] = useState([])
+    const {doRequest,errors} = useRequest({method:'post',url:'/api/users/signup', body:{
+        email, password
+    }})
     const onSubmit = async (event) => {
         event.preventDefault()
-
-
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            })
-            console.log(response.data)
-        } catch (error) {
-
-            console.log(error)
-            setErrors(error.response.data.errors)
-
-        }
-
-
-
+        doRequest()
     }
     return <form onSubmit={onSubmit}>
         <h1>Sign up</h1>
@@ -35,13 +23,9 @@ export default () => {
             <label>Password</label>
             <input className="form-control" type="password" value={password} onChange={e => setPassword(e.target.value)} />
         </div>
-        <div className="alert alert-danger">
-            <h4>Opps....</h4>
-            <ul>
-             {errors.map((err,key) => <li key={key}>{err.message}</li>)}
-            </ul>
-           
-        </div>
+      
+           { errors}
+       
         <button className="btn btn-primary"> Sign up</button>
 
     </form>
