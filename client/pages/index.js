d55a32bbc8933196e71e32528cf0b798a0f99d82
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2020-10-04 22:59:53 -0500

    adding getInitialprops

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,17 @@
-import axios from 'axios'
+import buildClient from '../api/build-client'
 
 
 const LandingPage = ({currentUser}) =>{
     console.log('I am in the component',currentUser)
-    return <h1>Langing page</h1>
+    return currentUser ? <h1>You are signed id</h1> :<h1>You are not signed in</h1>
 }
 
-LandingPage.getInitialProps = async()=>{
+LandingPage.getInitialProps = async(context)=>{
 
-    const response = await axios.get('/api/users/currentuser')
+    const {data} = await buildClient(context).get('/api/users/currentuser')
+    return data
 
-    return response.data
+    
+
 }
 export default LandingPage
