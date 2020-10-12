054ec0e37a2991c044cab6692b4e73d7ef81fafe
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2020-10-12 00:42:44 -0500

    changing enviroment to descktop and adding logout feature

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import Router from 'next/router'
+import useRequest from '../../hooks/use-request'
+export default () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const { doRequest, errors } = useRequest({
+        method: 'post', url: '/api/users/signin', body: {
+            email, password
+        }, onSuccess: () => Router.push('/')
+    })
+    const onSubmit = async (event) => {
+        event.preventDefault()
+
+        await doRequest()
+
+    }
+    return <form onSubmit={onSubmit}>
+        <h1>Sign In</h1>
+        <div className="form-group">
+            <label>Email addres</label>
+            <input className="form-control" type="text" value={email} onChange={e => setEmail(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+            <label>Password</label>
+            <input className="form-control" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </div>
+
+        {errors}
+
+        <button className="btn btn-primary"> Sign In</button>
+
+    </form>
+}
