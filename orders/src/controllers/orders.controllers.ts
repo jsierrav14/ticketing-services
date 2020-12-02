09d0f29daaa731c78a3fe110bef4ca08e402b0e1
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2020-12-02 07:44:43 -0500

    adding ticket model into orders services

--- orders/src/controllers/orders.controllers.ts
+++ orders/src/controllers/orders.controllers.ts
@@ -1,12 +1,45 @@
-import {Response, Request} from 'express'
+import { Response, Request } from 'express'
+import { Ticket } from '../models/ticket.model'
+import { Order } from '../models/order.model'
+import { BadRequestError, NotFoundError, OrderStatus } from '@js-ecommerceapp/common';
 
-
+const EXPIRATION_WINDOW_SECONDS = 15*60
 class OrdersController {
 
 
-    async new(req:Request,res:Response){
 
-        res.send({})
+    async new(req: Request, res: Response) {
+        const { ticketId } = req.body;
+        const ticket = await Ticket.findById(ticketId)
+
+        if (!ticket) {
+            throw new NotFoundError()
+        }
+
+        const isReserved = await ticket.isReserved();
+
+        if (isReserved) {
+            throw new BadRequestError("Ticket is already reserved");
+        }
+
+
+        const expiration = new Date();
+        expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+
+
+        const order = Order.build({
+            userId: req.currentUser!.id,
+            status:OrderStatus.Created,
+            expiresAt:expiration,
+            ticket
+        })
+
+        await order.save();
+
+
+        res.status(201).send({})
+
+
     }
 }
 
--- orders/src/models/order.model.ts
+++ orders/src/models/order.model.ts
@@ -1,17 +1,19 @@
 import mongoose from 'mongoose'
 import {OrderStatus} from '@js-ecommerceapp/common'
+import { TicketDoc } from './ticket.model';
 
+export {OrderStatus};
 interface OrderAttrs {
     userId: string;
     status: OrderStatus;
-    expiresAt: string;
+    expiresAt: Date;
     ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
     userId: string;
     status: OrderStatus;
-    expiresAt: string;
+    expiresAt: Date;
     ticket: TicketDoc;
 }
 
@@ -27,7 +29,9 @@ const orderSchema = new mongoose.Schema({
     },
     status:{
         type: String,
-        required:true
+        required:true,
+        enum:Object.values(OrderStatus),
+        default:OrderStatus.Created
     },
     expiresAt:{
         type:mongoose.Schema.Types.Date
