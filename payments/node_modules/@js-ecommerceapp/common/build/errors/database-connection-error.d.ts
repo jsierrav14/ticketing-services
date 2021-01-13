8b97382477e9829542f47da266946766d8ee284b
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2021-01-13 07:47:05 -0500

    removing node-modules payments

--- payments/node_modules/@js-ecommerceapp/common/build/errors/database-connection-error.d.ts
+++
@@ -1,9 +0,0 @@
-import { CustomError } from './custon-error';
-export declare class DatabaseConnectionError extends CustomError {
-    statusCode: number;
-    reason: string;
-    constructor();
-    serializeErrors(): {
-        message: string;
-    }[];
-}
